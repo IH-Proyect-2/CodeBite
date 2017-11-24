d827e7b81c18e574417984bc1ec01e369e68a2fa
FranRom <maildefranz@gmail.com>
2017-11-24 14:24:06 +0100

    category error fixed

--- models/Pill.js
+++ models/Pill.js
@@ -4,7 +4,7 @@ const Schema   = mongoose.Schema;
 const pillSchema = new Schema({
   title: String,
   description: String,
-  category: {type: String, enum: ['AJAX', 'Algorithms','Arrays','Async & Callbacks','Canvas','Conditionals & Loops', 'Debugging','DOM','Express','Functions','JQuery','JSON','Libraries','Methods','Mongo','Mongoose','Node','Objects','Passport','Prototype','Testing']},
+  category: {type: String, enum: ['AJAX', 'Algorithms','Arrays','Async-Callbacks','Canvas','Conditionals-Loops', 'Debugging','DOM','Express','Functions','JQuery','JSON','Libraries','Methods','Mongo','Mongoose','Node','Objects','Passport','Prototype','Testing']},
   link: String,
   language: {type: String, enum: ['English', 'Spanish']}
   // comments: [{type:Schema.Types.ObjectId, ref:'comment'}],
